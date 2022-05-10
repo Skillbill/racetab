237e7bbeda0c31cf4600bbf3b34c63092584113d
biagio ruggiero <biagio.ruggiero@skillbill.it>
2022-05-10 18:24:53 +0200

    Racetab version 1.0.0 + README

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
 # racetab
+
+## what is it
+It's a script that allow to start different job sequentially o parallel each other every one inside it's tab
+
+## installation
+Enter [src](/src/) folder and do
+```
+npm install
+npm link .
+```
+
+Now you have ```racetab``` between executable bash script, if you want command line help just execute 
+```
+racetab -h
+```
+
+### to disinstall
+IDK why you have to do it, but is simple
+```
+npm -g rm racetab
+```
+
+## Config file
+Using json or yml format you can write what you want to start with ```racetab``` you can make how many config file you wish/need and start task starting from the one you specify calling racetab or the ```main``` one.
+
+Example in JSON format:
+
+```
+{
+  "workdir": "absolute_path_to_work_dir",
+  "tasks":[
+    {
+      "label": "main",
+      "launch": [
+        "test",
+        "altro test"
+      ]
+    },
+    {
+      "label": "test",
+      "cmd": "python"
+    },
+    {
+      "label": "altro test",
+      "cmd": "echo \"test\""
+    },
+    {
+      "label": "after",
+      "cmd": "the other task will start after my end",
+      "launch": [
+        "altro test",
+        "test"
+      ]
+    }
+  ]
+}
+```
+Example in yml format:
+```
+workdir: absolute_path_to_workdir
+tasks:
+- label: main
+  launch:
+  - test
+  - altro test
+- label: test
+  cmd: python
+- label: altro test
+  cmd: echo "test"
+- label: after
+  cmd: echo "the other task will start after my end"
+  launch:
+  - altro test
+  - test
+```
+As you can see, you can specify a workdir where all task will be executed and a list of task.
+
+Every task can have a ```cmd``` or ```launch``` or both, exists another option ```regex``` we will speak of it soon.
+
+When a task contain:
+- only ```cmd``` that command will be execute in it's tab
+- only ```launch``` that tasks will be started parallel to each other
+- both ```cmd``` and ```launch``` will be executed the command ```cmd``` and after its termination will be started the tasks specified by label in ```launch``` list
+- finally if ```cmd```, ```launch``` and ```regex``` are all inside a task, ```cmd``` get started, its output will be processed and when the specified regex will match, the others tasks inside ```launch``` will start parallely
+
+## simple launch
+As you can see, syntax is simple, let's go through, if you want to start running a task and nexts ones in a config file you can simple:
+```
+racetab -c absolute_or_relative_path_to_config_file -t task_label -d path_to_workdir
+``` 
+if you do
+```
+racetab -c absolute_or_relative_path_to_config_file -d path_to_workdir
+```
+task with label main will be started if exists.
+
+```-d``` option, is optional, it indicate a dir where tasks will be executed and if a workdir is in config file that one override this option.
+
+## user config
+Obviously you can save yours config file for use them more comfortably.
+
+You can create a folder
+```
+mkdir -p ~/.config/racetab
+```
+
+put there yours config file with extension json/yml
+
+than launch one of them 
+(e.g. main task of ~/.config/racetab/my_project.yml) easily running:
+```
+racetab -c my_project
+```
---
+++ src/index.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const fs = require('fs')
+const os = require("os");
+const path = require('path')
+const yaml = require('js-yaml');
+const { exec } = require('child_process');
+
+program
+  .description('shell script that allow to launch in different tab syncronus or sequentially group of script eventually waiting for an output based on config file in json or yml')
+  .version('1.0.0')
+  .requiredOption('-c, --config <path_config_file/task_nam>', 'task config file path')
+  .option('-f, --format <JSON/YML>', 'task config file name format', 'JSON')
+  .option('-t, --task <task label>', 'task to execute label', 'main')
+  .option('-d, --dir <path_dir>', 'relative or absolute path to dir where command in config file have to be run', './')
+  .option('--launch', 'internal option, used for know when to launch a new terminal window', false)
+  .option('--waiting', 'internal option, used for know when the script has to do launch command after end of current command', false)
+  .option('-r, --regex <regex>', 'internal option, regex to know when a command is ready', null)
+  .parse()
+
+const options = program.opts()
+const scriptName = 'racetab'
+let config = undefined
+
+if(!fs.existsSync(options.config) || !fs.lstatSync(options.config).isFile()){
+  const userConfig = `${os.homedir()}/.config/racetab/${options.config}`
+  if(fs.existsSync(`${userConfig}.json`)){
+    options.config = `${userConfig}.json`
+  }else if(fs.existsSync(`${userConfig}.yml`)){
+    options.config = `${userConfig}.yml`
+    options.format = 'yml'
+  }else{
+    console.log(`not valid config file or bad location/extension if it is in ${os.homedir()}/.config/racetab`)
+    process.exit()
+  }
+}else{
+  options.config = path.resolve(options.config)
+}
+
+switch(options.format.toUpperCase()){
+  case 'JSON':
+    config = JSON.parse(fs.readFileSync(options.config))
+    break
+  case 'YML':
+  case 'YAML':
+    config = yaml.load(fs.readFileSync(options.config))
+    break
+  default:
+    console.log('not supported format')
+    process.exit()
+}
+
+
+options.dir = path.resolve(config.workdir ? config.workdir : options.dir)
+
+const tasks = {}
+config.tasks.forEach(
+  task => tasks[task.label] = task
+)
+const task = tasks[options.task]
+const waitenter = 'echo \\"press enter to close\\" && read'
+const itSelfStringTask = (taskLabel) => `${scriptName} -c "${options.config}" -t "${taskLabel}" --launch -f "${options.format}" -d "${options.dir}"`
+const terminalItSelfTask = (taskLabel, newWindow = false) => `gnome-terminal --working-directory "${options.dir}" ${newWindow?'':'--tab'} -- ${itSelfStringTask(taskLabel)}`
+const commandLaunchString = (cmd) => `gnome-terminal --working-directory ${options.dir} --tab --title "${task.label}" -- bash -c "${cmd}`
+const escapeQuote = (string) => string.replace(RegExp('"','g'),'\\"')
+const escapeOthers = (string) => string.replace(RegExp('\`','g'),'\\`')
+
+if(options.launch){
+  if(options.regex){
+    const regex = RegExp(options.regex)
+    let launched = false
+    process.stdin.resume();
+    process.stdin.on('data', (data) => {
+      process.stdout.write(data)
+      if(!launched && regex.test(data) && task.launch){
+        launched = true
+        task.launch.forEach(taskLabel => exec(terminalItSelfTask(taskLabel)))
+      }
+    });
+    process.stdin.on('end', () => {
+      process.exit()
+    });
+  }else{
+    if(task.cmd && !task.launch){
+      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))}; ${waitenter}"`)
+    }else if((!task.cmd && task.launch) ||
+      (task.cmd && task.launch && options.waiting)
+    ){
+      task.launch.forEach(taskLabel => exec(terminalItSelfTask(taskLabel)))
+    }else if(task.cmd && task.launch && !task.regex){
+      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} && ${escapeQuote(itSelfStringTask(task.label))} --waiting; ${waitenter}" `)
+    }else if(task.cmd && task.launch && task.regex){
+      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} 2>&1| ${escapeQuote(itSelfStringTask(task.label))} --waiting --regex \"${task.regex}\"; ${waitenter}" `)
+    }else{
+      console.log('UNSUPPORTED options and task')
+      process.exit()
+    }
+  }
+}else{
+  exec(terminalItSelfTask(task.label, true))
+}
