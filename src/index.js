236439e959ad388b5395dbf35c75c98b704eac0f
biagio ruggiero <biagio.ruggiero@skillbill.it>
2022-05-26 16:01:53 +0200

    added options stop-propagation, execute and instruction

--- src/index.js
+++ src/index.js
@@ -12,16 +12,24 @@ program
   .version('1.0.0')
   .requiredOption('-c, --config <path_config_file/task_nam>', 'task config file path')
   .option('-f, --format <JSON/YML>', 'task config file name format', 'JSON')
-  .option('-t, --task <task label>', 'task to execute label', 'main')
+  .option('-t, --task <task_label>', 'task to execute label', 'main')
   .option('-d, --dir <path_dir>', 'relative or absolute path to dir where command in config file have to be run', './')
-  .option('--launch', 'internal option, used for know when to launch a new terminal window', false)
-  .option('--waiting', 'internal option, used for know when the script has to do launch command after end of current command', false)
+  .option('-l, --launch', 'internal option, used for know when to not launch a new terminal window', false)
+  .option('-w, --waiting', 'internal option, used for know when the script has to do launch command after end of current command', false)
   .option('-r, --regex <regex>', 'internal option, regex to know when a command is ready', null)
+  .option('-s, --stop-propagation', 'when setted if a task has both cmd and launch the launch part will be ignored ', false)
+  .option('-x, --execute <task_label>', 'shortcut for "-t <task_label> --launch"', undefined)
+  .option('-i, --instruction <command>', 'override task cmd', undefined)
   .parse()
 
 const options = program.opts()
 const scriptName = 'racetab'
 let config = undefined
+
+if(options.execute){
+  options.task = options.execute
+  options.launch = true
+}
 
 if(!fs.existsSync(options.config) || !fs.lstatSync(options.config).isFile()){
   const userConfig = `${os.homedir()}/.config/racetab/${options.config}`
@@ -59,9 +67,13 @@ config.tasks.forEach(
   task => tasks[task.label] = task
 )
 const task = tasks[options.task]
+if(options.instruction){
+  task.cmd = options.instruction
+}
+
 const waitenter = 'echo \\"press enter to close\\" && read'
-const itSelfStringTask = (taskLabel) => `${scriptName} -c "${options.config}" -t "${taskLabel}" --launch -f "${options.format}" -d "${options.dir}"`
-const terminalItSelfTask = (taskLabel, newWindow = false) => `gnome-terminal --working-directory "${options.dir}" ${newWindow?'--maximize':'--tab'} -- ${itSelfStringTask(taskLabel)}`
+const itSelfStringTask = (taskLabel) => `${scriptName} -c "${options.config}" -x "${taskLabel}" -f "${options.format}" -d "${options.dir}" ${options.stopPropagation ? '-s':''}`
+const terminalItSelfTask = (taskLabel, newWindow = false, instruction = false) => `gnome-terminal --working-directory "${options.dir}" ${newWindow?'--maximize':'--tab'} -- ${itSelfStringTask(taskLabel)} ${instruction ? `-i "${options.instruction}"` : ""}`
 const commandLaunchString = (cmd) => `gnome-terminal --working-directory ${options.dir} --tab --title "${task.label}" -- bash -c "${cmd}`
 const escapeQuote = (string) => string.replace(RegExp('"','g'),'\\"')
 const escapeOthers = (string) => string.replace(RegExp('\`','g'),'\\`')
@@ -82,21 +94,21 @@ if(options.launch){
       process.exit()
     });
   }else{
-    if(task.cmd && !task.launch){
+    if(task.cmd && (!task.launch || options.stopPropagation)){
       exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))}; ${waitenter}"`)
     }else if((!task.cmd && task.launch) ||
       (task.cmd && task.launch && options.waiting)
     ){
       task.launch.forEach(taskLabel => exec(terminalItSelfTask(taskLabel)))
     }else if(task.cmd && task.launch && !task.regex){
-      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} && ${escapeQuote(itSelfStringTask(task.label))} --waiting; ${waitenter}" `)
+      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} && ${escapeQuote(itSelfStringTask(task.label))} -w; ${waitenter}" `)
     }else if(task.cmd && task.launch && task.regex){
-      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} 2>&1| ${escapeQuote(itSelfStringTask(task.label))} --waiting --regex \"${task.regex}\"; ${waitenter}" `)
+      exec(`${commandLaunchString(escapeOthers(escapeQuote(task.cmd)))} 2>&1| ${escapeQuote(itSelfStringTask(task.label))} -w -r \"${task.regex}\"; ${waitenter}" `)
     }else{
       console.log('UNSUPPORTED options and task')
       process.exit()
     }
   }
 }else{
-  exec(terminalItSelfTask(task.label, true))
+  exec(terminalItSelfTask(task.label, true, options.instruction !== undefined))
 }
